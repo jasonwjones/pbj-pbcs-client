3192c758641ea9900f012ce402fc39bfedffce64
Jason Jones <jasonwjones@gmail.com>
2022-06-16 13:28:37 -0700

    Copy constructor for grid

--- src/main/java/com/jasonwjones/pbcs/client/impl/HashMapGrid.java
+++ src/main/java/com/jasonwjones/pbcs/client/impl/HashMapGrid.java
@@ -13,6 +13,24 @@ public class HashMapGrid<E> implements Grid<E> {
 
 	private final Map<Integer, E> data;
 
+	public HashMapGrid(Grid<E> grid) {
+		if (grid instanceof HashMapGrid) {
+			HashMapGrid<E> hashMapGrid = (HashMapGrid<E>) grid;
+			this.rows = hashMapGrid.rows;
+			this.columns = hashMapGrid.columns;
+			this.data = new HashMap<>(hashMapGrid.data);
+		} else {
+			this.rows = grid.getRows();
+			this.columns = grid.getColumns();
+			data = new HashMap<>(rows * columns);
+			for (int row = 0; row < grid.getRows(); row++) {
+				for (int col = 0; col < grid.getColumns(); col++) {
+					setCell(row, col, grid.getCell(row, col));
+				}
+			}
+		}
+	}
+
 	public HashMapGrid(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
