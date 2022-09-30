c4fe66bfc2751d072a7e831f1fdb26ea74eace30
Jason Jones <jasonwjones@gmail.com>
2022-09-30 08:06:24 -0700

    New exception subclass for auth errors

---
+++ src/main/java/com/jasonwjones/pbcs/client/exceptions/PbcsInvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package com.jasonwjones.pbcs.client.exceptions;
+
+public class PbcsInvalidCredentialsException extends PbcsClientException {
+
+    public PbcsInvalidCredentialsException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/com/jasonwjones/pbcs/client/impl/MyResponseErrorHandler.java
+++ src/main/java/com/jasonwjones/pbcs/client/impl/MyResponseErrorHandler.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
+import com.jasonwjones.pbcs.client.exceptions.PbcsInvalidCredentialsException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -14,21 +15,21 @@ import org.springframework.web.client.ResponseErrorHandler;
 import com.jasonwjones.pbcs.client.exceptions.PbcsClientException;
 
 public class MyResponseErrorHandler implements ResponseErrorHandler {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MyResponseErrorHandler.class);
 
 	@Override
 	public void handleError(ClientHttpResponse response) throws IOException {
 		//logger.error("Response error: {} {}", response.getStatusCode(), response.getStatusText());
-				
+
 		// pull the response body and pass separately since the first read of the stream off of
 		// ClientHttpResponse would eat it and make subsequent attempts fail
 		String responseBody = inputStreamToString(response.getBody());
-		
+
 //		logger.error("Headers: {}", response.getHeaders());
 //		logger.error("Status: {} ({})", response.getStatusCode().value(), response.getStatusText());
 //		logger.error("Body: {}", responseBody);
-		
+
 		if (response.getStatusCode().value() == 404) {
 			throw new PbcsClientException("Couldn't find endpoint");
 		} else if (response.getStatusCode().value() == 400) { // Bad Request
@@ -37,7 +38,9 @@ public class MyResponseErrorHandler implements ResponseErrorHandler {
 		} else if (response.getStatusCode().value() == 503) {
 			throw new PbcsClientException("Service currently unavailable; likely in maintenance mode");
 		} else if (response.getStatusCode().value() == 401) {
-			throw new PbcsClientException("Unable to login to PBCS due to invalid credentials");
+			// TODO: there is a little more info coming back on the www-authenticate header that might be useful to
+			// show: Bearer error="invalid_token", error_description="Token Expired"
+			throw new PbcsInvalidCredentialsException("Unable to login to PBCS due to invalid credentials");
 		}
 	}
 
@@ -51,7 +54,7 @@ public class MyResponseErrorHandler implements ResponseErrorHandler {
 		return (HttpStatus.Series.CLIENT_ERROR.equals(series)
 				|| HttpStatus.Series.SERVER_ERROR.equals(series));
 	}
-	
+
 	private static String inputStreamToString(InputStream inputStream) throws IOException  {
 		final int bufferSize = 1024;
 		final char[] buffer = new char[bufferSize];
@@ -65,5 +68,5 @@ public class MyResponseErrorHandler implements ResponseErrorHandler {
 		}
 		return out.toString();
 	}
-	
+
 }
