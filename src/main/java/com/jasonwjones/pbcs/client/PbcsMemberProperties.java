bd0f0317b1cfd03a9ed62887cff7ae6df287dd83
Jason Jones <jasonwjones@gmail.com>
2022-10-26 15:16:34 -0700

    Fill in additional info on PbcsMemberProperties

--- src/main/java/com/jasonwjones/pbcs/client/PbcsMemberProperties.java
+++ src/main/java/com/jasonwjones/pbcs/client/PbcsMemberProperties.java
@@ -3,6 +3,7 @@ package com.jasonwjones.pbcs.client;
 import java.util.List;
 
 // TODO: rename to just PbcsMember
+// TODO: compare payload vs properties here: "{"dataStorage":"Store Data","dimName":"Scenario","usedIn":["Plan1","Vision"],"invalidDueToValidIntersection":false,"objectType":31,"twoPass":false,"displayPath":"/Scenario/Actual","generation":2,"aliasPath":"/Scenario/Actual","level":0,"oldName":"Actual","dataType":"Unspecified","valid":true,"parentName":"Scenario","name":"Actual","id":"f5aa5599-da40-4224-9ee8-4d77503261a8","path":"/Scenario/Actual","links":[{"rel":"self","href":"https://appliedolapepm-test-appliedolapepm.epm.us-phoenix-1.ocs.oraclecloud.com:443/HyperionPlanning/rest/v3/applications/Vision/dimensions/Scenario/members/Actual","action":"GET"}]}[\r][\n]"
 public interface PbcsMemberProperties {
 
 	public String getName();
@@ -27,15 +28,24 @@ public interface PbcsMemberProperties {
 
 	public String getParentName();
 
+	// Unspecified, ...?
 	public String getDataType();
 
-	// such as 33
-	// 32
+	// seems to be the object types from the HSP table:
+	// https://www.epmmarshall.com/the-planning-repository-hsp_object-and-hsp_object_type/
+
+	// Confirmed values:
+	// 31: Scenario
+	// 32: Account
+	// 33: Entity
+	// 34: Time Period (also seen on member such as H-T-D)
+	// 35: Version
+	// 38: Year
 	public Integer getObjectType();
 
 	// such as STOREDATA
 	// TODO: enum
-	// observed values: Never Share, Dynamic Calc, Store Data, Label Only
+	// observed values: Never Share, Dynamic Calc, Store Data, Label Only, Dynamic Calc and Store
 	public String getDataStorage();
 
 	// mapped from dimName
