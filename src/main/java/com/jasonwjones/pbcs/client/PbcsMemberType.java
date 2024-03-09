339c0b3aa05464d367d938d71f03f09bafccbe3d
Jason Jones <jasonwjones@gmail.com>
2024-03-08 22:57:34 -0400

    Add attribute and user-defined member (dimension) types

--- src/main/java/com/jasonwjones/pbcs/client/PbcsMemberType.java
+++ src/main/java/com/jasonwjones/pbcs/client/PbcsMemberType.java
@@ -5,7 +5,6 @@ package com.jasonwjones.pbcs.client;
  * <a href="http://www.hyperionepm.com/archives/102">...</a>
  *
  * @author Jason Jones
- *
  */
 public enum PbcsMemberType {
 
@@ -14,6 +13,8 @@ public enum PbcsMemberType {
 	 * an object type we don't have a mapping for
 	 */
 	UNKNOWN(0, "Unknown"), // not a real row in table, just give
+
+	ATTRIBUTE(30, "Attribute"),
 
 	SCENARIO(31, "Scenario"),
 
@@ -30,7 +31,17 @@ public enum PbcsMemberType {
 	// note that the Currency DIMENSION member itself seems to report a type of 9 while members in it are 37
 	CURRENCY(37, "Currency"),
 
-	SHARED(45, "Shared");
+	/**
+	 * Represents a shared member.
+	 */
+	SHARED(45, "Shared"),
+
+	/**
+	 * No specific dimension type. For example, the Product dimension would fall into this category.
+	 * The full legacy text description (i.e. from <code>HSP_OBJECT</code> text is <code>User Defined Dimension Member</code>
+	 * but for slight consistency it has been trimmed here.
+	 */
+	USER_DEFINED(50, "User Defined");
 
 	private final int objectTypeId;
 
@@ -41,14 +52,35 @@ public enum PbcsMemberType {
 		this.typeName = typeName;
 	}
 
+	/**
+	 * Gets the internal object type ID. This more or less corresponds to the legacy object
+	 * type ID from the <code>HSP_OBJECT</code> table in the Planning repository. This
+	 * information is ostensibly included for posterity, it shouldn't be used for anything.
+	 *
+	 * @return the internal object type ID.
+	 */
 	public int getObjectTypeId() {
 		return objectTypeId;
 	}
 
+	/**
+	 * Returns a friendly description of th member/object type name, which is essentially the
+	 * ordinal name but in title case and spaces instead of underscores.
+	 *
+	 * @return the type description
+	 */
 	public String getTypeName() {
 		return typeName;
 	}
 
+	/**
+	 * Parses an object type Id into a enum member. If the type cannot be determined, then this
+	 * method will return {@link #UNKNOWN}, which isn't a real type, but is included so that some
+	 * heretofore unknown type name won't throw an exception.
+	 *
+	 * @param objectTypeId the numeric type value, such as from a get member info response payload
+	 * @return the type that corresponds to that numeric type, {@link #UNKNOWN} if it's unknown.
+	 */
 	public static PbcsMemberType valueOf(int objectTypeId) {
 		for (PbcsMemberType id : PbcsMemberType.values()) {
 			if (id.getObjectTypeId() == objectTypeId) {
