62fb69a6005b9d801a866b606f4f192194c404fe
Jason Jones <jasonwjones@gmail.com>
2024-03-15 09:32:38 -0700

    Add PlanTypeWalker utility

---
+++ src/main/java/com/jasonwjones/pbcs/util/PlanTypeWalker.java
@@ -0,0 +1,130 @@
+package com.jasonwjones.pbcs.util;
+
+import com.jasonwjones.pbcs.client.PbcsDimension;
+import com.jasonwjones.pbcs.client.PbcsMemberProperties;
+import com.jasonwjones.pbcs.client.PbcsPlanType;
+import org.springframework.util.StringUtils;
+
+import java.io.PrintStream;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Queue;
+
+/**
+ * Provides a generic way to walk the outline of a given plan.
+ */
+public class PlanTypeWalker {
+
+    private PlanTypeWalker() {}
+
+    /**
+     * Walks all the dimensions in the given plan, calling the visitor for various events when starting to process the
+     * overall plan, dimensions, and members.
+     *
+     * @param planType the plan to walk
+     * @param visitor the visitor delegate to call
+     */
+    public static void walk(PbcsPlanType planType, Visitor visitor) {
+        visitor.startPlan(planType);
+
+        for (PbcsDimension dimension : planType.getDimensions()) {
+            if (visitor.startDimension(dimension) == MemberVisitResult.CONTINUE) {
+                Queue<PbcsMemberProperties> members = new ArrayDeque<>();
+                members.add(dimension.getRoot());
+
+                while (!members.isEmpty()) {
+                    PbcsMemberProperties current = members.remove();
+                    if (visitor.visitMember(current) == MemberVisitResult.CONTINUE) {
+                        members.addAll(current.getChildren());
+                    }
+                }
+
+                visitor.endDimension(dimension);
+            }
+        }
+        visitor.endPlan(planType);
+    }
+
+    /**
+     * A visitor with events that are called for various items being processed.
+     */
+    public interface Visitor {
+
+        void startPlan(PbcsPlanType plan);
+
+        void endPlan(PbcsPlanType plan);
+
+        MemberVisitResult startDimension(PbcsDimension dimension);
+
+        void endDimension(PbcsDimension dimension);
+
+        MemberVisitResult visitMember(PbcsMemberProperties member);
+
+    }
+
+    public enum MemberVisitResult {
+
+        CONTINUE,
+
+        TERMINATE
+
+    }
+
+    /**
+     * A base class that can be extended by those implementing the Visitor interface. All methods are stubbed out so
+     * that implementers only need to worry about method they care about.
+     */
+    public static abstract class AbstractVisitor implements Visitor {
+
+        @Override
+        public void startPlan(PbcsPlanType plan) {
+        }
+
+        @Override
+        public void endPlan(PbcsPlanType plan) {
+        }
+
+        @Override
+        public MemberVisitResult startDimension(PbcsDimension dimension) {
+            return MemberVisitResult.CONTINUE;
+        }
+
+        @Override
+        public void endDimension(PbcsDimension dimension) {
+        }
+
+        @Override
+        public MemberVisitResult visitMember(PbcsMemberProperties member) {
+            return MemberVisitResult.CONTINUE;
+        }
+
+    }
+
+    public static class PrinterVisitor extends AbstractVisitor implements Visitor {
+
+        private final PrintStream printStream;
+
+        private static final int SPACES_PER_LEVEL = 4;
+
+        public PrinterVisitor() {
+            this(System.out);
+        }
+
+        public PrinterVisitor(PrintStream printStream) {
+            this.printStream = printStream;
+        }
+
+        @Override
+        public MemberVisitResult visitMember(PbcsMemberProperties member) {
+            int spaces = (member.getGeneration() - 1) * SPACES_PER_LEVEL;
+            printStream.println(space(spaces) + member.getName());
+            return MemberVisitResult.CONTINUE;
+        }
+
+        private static String space(int size) {
+            return String.join("", Collections.nCopies(size, " "));
+        }
+
+    }
+
+}
---
+++ src/test/java/com/jasonwjones/pbcs/util/PlanTypeWalkerTest.java
@@ -0,0 +1,38 @@
+package com.jasonwjones.pbcs.util;
+
+import com.jasonwjones.pbcs.client.PbcsApplication;
+import com.jasonwjones.pbcs.client.PbcsPlanType;
+import com.jasonwjones.pbcs.client.impl.PlanTypeConfigurationImpl;
+import com.jasonwjones.pbcs.utils.PbcsClientUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PlanTypeWalkerTest {
+
+    private PbcsPlanType plan;
+
+    public static final List<String> DIMENSIONS = Arrays.asList("Account", "Currency", "Entity", "Period", "Product", "Scenario", "Version", "Year");
+
+    @Before
+    public void setUp() {
+        PbcsApplication app = PbcsClientUtils.vision();
+
+        PbcsApplication.PlanTypeConfiguration configuration = new PlanTypeConfigurationImpl.Builder("Plan1")
+                .skipCheck()
+                .dimensions(DIMENSIONS)
+                .build();
+
+        plan = app.getPlanType(configuration);
+    }
+
+    @Test
+    public void walk() {
+        PlanTypeWalker.walk(plan, new PlanTypeWalker.PrinterVisitor());
+    }
+
+}
