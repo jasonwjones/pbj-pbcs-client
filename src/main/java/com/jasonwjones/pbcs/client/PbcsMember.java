076e3cad43ec0267ba30d94832e5cca1dc28aa77
Jason Jones <jasonwjones@gmail.com>
2024-06-15 09:12:27 -0700

    Cleanup javadoc

--- src/main/java/com/jasonwjones/pbcs/client/PbcsMember.java
+++ src/main/java/com/jasonwjones/pbcs/client/PbcsMember.java
@@ -40,14 +40,12 @@ public interface PbcsMember extends PbcsObject {
     List<PbcsMember> getChildren();
 
     /**
-     * Convenience method that checks whether the given member is a leaf node
-     * (level-0) or not. Effectively this should always be true in the case that
-     * <code>getChildren().size() == 0</code>. This should also be true in case
-     * <code>getChildren()</code> returns null, although valid implementations of
-     * this interface should return an empty collection instead of null.
+     * Convenience method that checks whether the given member is a leaf node (level-0) or not. Effectively this should
+     * always be true in the case that <code>getChildren().size() == 0</code>. This should also be true in case
+     * <code>getChildren()</code> returns null, although valid implementations of this interface should return an empty
+     * collection instead of null.
      *
-     * @return true if this member has no children and is a level-0/leaf node,
-     *         false otherwise.
+     * @return true if this member has no children and is a level-0/leaf node, false otherwise.
      */
     boolean isLeaf();
 
